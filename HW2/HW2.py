05b698706b94541737f459b8b2c392e990cc41c1
rx9933 <arushi.sadam@gmail.com>
2024-02-14 00:14:48 -0600

    adding cholesky bonus p1

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.linalg import lu
-
+from math import sqrt
 # PROBLEM 6
 def naive_LU(A):
     """
@@ -44,19 +44,50 @@ def solve_LU(L,U,b):
     return x
 
 # BONUS, Part 1
+# def my_Cholesky(A):
+#     shape = A.shape
+#     if shape[0]!=shape[1]:
+#         print("input is not a square matrix")
+#         return 1
+#     if A.T.any() != A.any(): # is symmetric
+#         print("input is a square but not a symmetric matrix")
+#         return 1
+    # U = np.zeros(shape)
+    # for i in range(shape[0]):
+    #     for j in range(i+1, shape[0]):
+    #         sum = 0
+    #         for k in range(i-1):
+    #             sum += U[k,i] * U[k,j]
+    #         U[i,j] = A[i,j] - sum
+    # for i in range(shape[0]): # each row
+    #     # along diagonal
+    #     ukisq = 0
+    #     for k in range(1, i-1):
+    #         ukisq += U[k,i]**2
+    #     U[i,i] = sqrt(A[i,i]-ukisq)
+    # print(U.T)
 def my_Cholesky(A):
     shape = A.shape
-    if shape[0]!=shape[1]:
-        print("input is not a square matrix")
-        return 1
-    if A.T != A: # is symmetric
-        print("input is a square but not a symmetric matrix")
-        return 1
+    if shape[0] != shape[1]:
+        print("Input is not a square matrix")
+        return None
+    if not np.allclose(A.T, A):  # Check for symmetry
+        print("Input is a square matrix but not symmetric")
+        return None
+    n = shape[0]
+  
+    U = np.zeros_like(A, dtype=np.float64)
     
+    for i in range(n):
+        for j in range(i, n):
+            if i == j:
+                # Diagonal elements
+                U[i, i] = np.sqrt(A[i, i] - np.sum(U[:i, i]**2))
+            else:
+                # Non-diagonal elements
+                U[i, j] = (A[i, j] - np.sum(U[:i, i] * U[:i, j])) / U[i, i]
+    return U
     
-
-
-
 
 # PROBLEM 7
 """
@@ -94,8 +125,36 @@ L,U = naive_LU(A)
 # print(np.matmul(np.array([[1,0,0],[-3/10,1,0],[1/10,-4/27,1]]), np.array([[10,2,-1],[0,-5.4,1.7],[0,0,289/54]])))
 
 # PROBLEM 4
-b = np.array([27,-61.5,-21.5])
-print(solve_LU(L,U, b))
+# b = np.array([27,-61.5,-21.5])
+# print(solve_LU(L,U, b))
 
-b = np.array([12,18,-6])
-print(solve_LU(L,U, b))
+# b = np.array([12,18,-6])
+# print(solve_LU(L,U, b))
+
+# PROBLEM 5
+A = np.array([[2,-6,-1],[-3,-1,7],[-8,1,-2]])
+b = np.array([-38,-34,-40])
+
+# print(np.linalg.solve(A,b))
+# p,l,u = lu(A)
+# print(np.linalg.inv(p)@A)
+# print(l@u)
+P = np.array([[0,0,1],[1,0,0],[0,1,0]])
+L = np.array([[1,0,0],[-1/4,1,0],[3/8,11/46,1]])
+U = np.array([[-8,1,-2], [0, -23/4, -3/2], [0,0,373/46]])
+# print(np.matmul(P,A))
+# print(np.matmul(L,U))
+
+# p2 = np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]])
+# l1 = np.array([[1, 0, 0], [-3/8, 1, 0], [1/4, 0, 1]])
+# l2 = np.array ([[1, 0, 0], [0, 1, 0], [0, -11/46, 0]])
+
+# a1 = np.matmul(p2,np.linalg.inv(l1))
+# a2 = np.matmul(a1, np.linalg.inv(p2))
+# a3 = np.matmul (a2, np.linalg.inv(l2))
+
+# BONUS p.1
+# A = np.array([[4, 12, -16],[12, 37, -43], [-16, -43, 98]])
+A = np.array([[6, 15, 55], [15, 55, 225], [55, 225, 979]])
+print(my_Cholesky(A))
+print(np.linalg.cholesky(A).T) # RETURNS L
